57bfe0a36eb162ff910a55c4e023bf8b10e2b4a2
Abdnl <abdurrahmaniscan@gmail.com>
2021-06-12 20:32:27 +0200

    doing more work after initially publishing to GitHub

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -6,6 +6,11 @@ public class Practice1 {
 
         System.out.println("WORK 1");
 
+        // Successfully pushed project to GitHub
+            // first Intellij automatically created remote repo
+                // created connection between local repo and remote repo
+                   //pushed local commits to remote repo
+
 
 
     }
